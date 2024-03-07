a40b7e08265e50e7cff2e06432e4810acf63454d
siddhuthecoder <110759676+siddhuthecoder@users.noreply.github.com>
2024-03-07 21:28:26 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,37 @@
+import team1 from "../assets/webteam/sid.png";
 
-import test from '../assets/webteam/sid.png';
+export const navigation = [
+  {
+    id: 1,
+    name: "Events",
+    link: "/events",
+  },
+  {
+    id: 2,
+    name: "Workshops",
+    link: "/workshops",
+  },
+  {
+    id: 3,
+    name: "Core Team",
+    link: "/coreteam",
+  },
+  {
+    id: 4,
+    name: "Web Team",
+    link: "/webteam",
+  },
+  {
+    id: 5,
+    name: "About",
+    link: "/about",
+  },
+  {
+    id: 6,
+    name: "Events",
+    link: "/events",
+  },
+];
 
 
 export const WebTeamCard = [
@@ -8,25 +40,25 @@ export const WebTeamCard = [
     id: 1,
     name: "Aswini Kodati",
     role: "UI/UX Designer",
-    img: test,
+    img: team1,
   },
   {
     id: 1,
     name: "Yasvanth Hanumantu",
     role: "Web Developer",
-    img: test,
+    img: team1,
   },
   {
     id: 3,
     name: "Sribabu Mandraju",
     role: "Web Developer",
-    img: test,
+    img: team1,
   },
   {
     id: 4,
     name: "Hemanth",
     role: "Web Developer",
-    img: test,
+    img: team,
   },
 ];
 
@@ -36,19 +68,19 @@ export const CoreTeamData = [
     id: 1,
     name: "Siddhu From Srikakulam",
     role: "UI/UX Designer",
-    img: test,
+    img: team1,
   },
   {
     id: 2,
     name: "Siddhu From Srikakulam",
     role: "Web Developer",
-    img: test,
+    img: team1,
   },
   {
     id: 3,
     name: "Siddhu From Srikakulam",
     role: "Web Developer",
-    img: test,
+    img: team1,
   },
   {
     id: 4,
@@ -60,24 +92,24 @@ export const CoreTeamData = [
     id: 5,
     name: "Siddhu From Srikakulam",
     role: "UI/UX Designer",
-    img: test,
+    img: team1,
   },
   {
     id: 6,
     name: "Siddhu From Srikakulam",
     role: "Web Developer",
-    img: test,
+    img: team1,
   },
   {
     id: 7,
     name: "Siddhu From Srikakulam",
     role: "Web Developer",
-    img: test,
+    img: team1,
   },
   {
     id: 8,
     name: "Siddhu From Srikakulam",
     role: "Web Developer",
-    img: test,
+    img: team1,
   },
 ];
