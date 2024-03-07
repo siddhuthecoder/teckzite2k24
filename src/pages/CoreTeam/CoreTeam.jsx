23e51e25be9af0cf0c5010f9e52a51a0e1249f3a
siddhuthecoder <110759676+siddhuthecoder@users.noreply.github.com>
2024-03-07 21:06:13 +0530

    Update CoreTeam.jsx

--- src/pages/CoreTeam/CoreTeam.jsx
+++ src/pages/CoreTeam/CoreTeam.jsx
@@ -1,17 +1,18 @@
-import TeamCard from '../../components/Shared/TeamCard';
-import Style from './CoreTeam.module.css';
+// import TeamCard from '../../components/Shared/TeamCard';
+import WebCard from '../../components/Shared/WebCard'
 
+import { CoreTeamData } from "../../constants";
 const CoreTeam = () => {
   return (
-      <div>
-        <div className="flex flex-wrap max-sm:justify-center max-xl:justify-around justify-between">
-          <TeamCard />
-          <TeamCard />
-          <TeamCard />
-          <TeamCard />
-          <TeamCard />
-        </div>
-      </div>
+    <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4   ">
+      {CoreTeamData.map((t) => (
+          <div key={t.id} className="flex justify-center my-5">
+            <WebCard img={t.img} name={t.name} role={t.role} />
+          </div>
+        ))}
+       </div>
+    </div>
   )
 }
 
