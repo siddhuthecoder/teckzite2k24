dc130f723c77f3e6977fadfde40f3586337b7ff1
siddhuthecoder <110759676+siddhuthecoder@users.noreply.github.com>
2024-03-07 21:33:31 +0530

    Update CoreTeam.jsx

--- src/pages/CoreTeam/CoreTeam.jsx
+++ src/pages/CoreTeam/CoreTeam.jsx
@@ -4,7 +4,7 @@ import WebCard from '../../components/Shared/WebCard'
 import { CoreTeamData } from "../../constants";
 const CoreTeam = () => {
   return (
-    <div>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4   ">
       {CoreTeamData.map((t) => (
           <div key={t.id} className="flex justify-center my-5">
